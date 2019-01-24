26fff68c965c7f42377cc2cd137260c1e61528c5
DedKo80 <aksenovms@gmail.com>
2019-01-24 18:47:48 +0700

    Реализована клиентская часть

--- gibbet_client.py
+++ gibbet_client.py
@@ -20,5 +20,38 @@ try:
     receved = sock.recv(1024).decode()
     print(receved)
 except:
-    pass
+    print('Ошибка отправки данных на сервер {}:{}'.format(HOST, PORT))
+    sock.close()
+
+data = receved.split(';')
+if data[0] == 'GUESS':
+    print('Угадайте число от {} до {}'.format(data[1], data[2]))
+    while True:
+        x = input('Ваш ответ (для выхода введите q)')
+        if x == 'q':
+            sock.sendall(bytes('GOODBYE', 'utf-8'))
+            break
+        try:
+            sock.sendall(bytes('TRY;{}'.format(x), 'utf-8'))
+            receved = sock.recv(1024).decode()
+        except:
+            print('Ошибка отправки числа на сервер {}:{}'.format(HOST, PORT))
+            break
+        data = receved.split(';')
+        if data[0] == 'TRUE':
+            print('Вы угадали !!!')
+            break
+        elif data[0] == 'FALSE':
+            if data[2] == '<':
+                print('Не угадал! Число меньше. Осталось {} попыток'.format(data[1]))
+            else:
+                print('Не угадал! Число больше. Осталось {} попыток'.format(data[1]))
+        elif data[0] == 'FAIL':
+            print('Вы проиграли у-ха-ха')
+            break
+else:
+    print('не известаный ответ сервера')
+
+sock.close()
+
 
